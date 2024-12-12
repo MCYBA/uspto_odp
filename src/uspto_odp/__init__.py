34d8e4d68655c316909d93ba5c48ae545f296159
Ken Thompson <kenthompson1@gmail.com>
2024-12-12 12:43:03 -0500

    further continuity logic changes

--- src/uspto_odp/__init__.py
+++ src/uspto_odp/__init__.py
@@ -1,6 +1,12 @@
 from . import controller
 from . import models
 from importlib.metadata import version, PackageNotFoundError
+from uspto_odp.models.patent_continuity import (
+    ParentContinuity,
+    ChildContinuity, 
+    ContinuityCollection,
+    ContinuityData,
+)
 
 try:
     __version__ = version("uspto_odp")
@@ -8,4 +14,12 @@ except PackageNotFoundError:  # pragma: no cover
     # Package is not installed
     __version__ = "unknown"
 
-__all__ = ['controller', 'models', '__version__']
+__all__ = [
+    'controller',
+    'models',
+    '__version__',
+    'ParentContinuity',
+    'ChildContinuity',
+    'ContinuityCollection',
+    'ContinuityData',
+]
